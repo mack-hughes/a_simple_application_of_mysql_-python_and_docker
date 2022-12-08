16ba8937c42d3ea5c5986552ac2bf49c658f7487
mack hughes <mackhughes251@gmail.com>
2022-12-08 18:09:09 -0300

    .

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from .entities import Users as UsersModel
 
 class UserRepo:
     def insert_user(self, name):
-        with DBCOnnection() as db:
+        with DBConnection() as db:
             new_user = UsersModel(name=name)
             db.session.add(new_user)
             db.session.commit()
